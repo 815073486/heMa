964fd8589eb9787d2d25f2694b63f28666a331b7
815073486 <815073486@qq.com>
2020-04-08 18:29:44 +0800

    Initial commit

---
+++ hema-index/src/main/java/com/hema/HemaIndexApplication.java
@@ -0,0 +1,13 @@
+package com.hema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemaIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemaIndexApplication.class, args);
+    }
+
+}
---
+++ hema-index/src/test/java/com/hema/HemaIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemaIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
